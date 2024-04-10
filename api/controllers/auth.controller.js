bf7271d5bef0fb31e1601c3dc2f17452bac3c0e1
Girish Girijan <girishvazheliparambil@gmail.com>
2024-04-10 14:47:29 -0300

    Added middleware and a function to handle custom errors

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,15 +1,28 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs'
+import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
-  const { username, email, password } = req.body;
-  const hashedPassword = bcryptjs.hashSync(password, 10);
-  const newUser = new User({ username, email, password: hashedPassword });
-
+export const signup = async (req, res, next) => {
   try {
-    await newUser.save();
-    res.status(201).json({ message: "User created successfully!" });
+    const { username, email, password } = req.body;
+
+    //Checking whether username is already exist
+    const existingUsername = await User.findOne({ username });
+    if (existingUsername) {
+      res.status(500).json({ message: "Username is already exist" });
+    } else {
+      //Checking whether email id is already exist
+      const existingEmail = await User.findOne({ email });
+      if (existingEmail) {
+        res.status(500).json({ message: "Email is already exist" });
+      } else {
+        const hashedPassword = bcryptjs.hashSync(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+
+        await newUser.save();
+        res.status(201).json({ message: "User created successfully" });
+      }
+    }
   } catch (error) {
-    res.status(500).json(error.message);
+    next(error)
   }
 };
