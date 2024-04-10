b77a453642e19f0c1ab81291ccbeb8043a53e22a
Girish Girijan <girishvazheliparambil@gmail.com>
2024-04-10 13:21:28 -0300

    Created User model

--- api/index.js
+++ api/index.js
@@ -1,4 +1,13 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+
+mongoose.connect(process.env.DB_URL).then(() => {
+    console.log("Connected to MongoDB")
+}).catch((err) => {
+    console.log("Error: ", err)
+});
 
 const app = express();
 
