7c84a00edddefd63aee459a4b30e25f85f7b0870
Girish Girijan <girishvazheliparambil@gmail.com>
2024-04-17 13:12:14 -0300

    Added pre production changes

--- api/index.js
+++ api/index.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
-import cors from 'cors'
+import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 dotenv.config();
 
@@ -17,10 +18,17 @@ mongoose
     console.log("Error: ", err);
   });
 
+const __dirname = path.resolve();
 const app = express();
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html")); // '/client/dist/index.html'
+});
+
 app.use(express.json());
-app.use(cors())
-app.use(cookieParser())
+app.use(cors());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is listening on port 3000");
